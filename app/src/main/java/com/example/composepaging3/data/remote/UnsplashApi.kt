1f82317901d8d530e1de3b19d411b421fd9bfbac
mayankkasera <mayankkassera98@gmail.com>
2022-10-01 17:16:17 +0530

    UnsplashApi end point added.

---
+++ app/src/main/java/com/example/composepaging3/data/remote/UnsplashApi.kt
@@ -0,0 +1,27 @@
+package com.example.composepaging3.data.remote
+
+
+import com.example.composepaging3.BuildConfig
+import com.example.composepaging3.data.model.SearchResult
+import com.example.composepaging3.data.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("query") query: String,
+        @Query("per_page") perPage: Int
+    ): SearchResult
+
+}
