54cb8a195ab6d51ffd18bd5eb8261193cd1a9552
mayankkasera <mayankkassera98@gmail.com>
2022-10-01 17:44:44 +0530

    Di Module added.

---
+++ app/src/main/java/com/example/composepaging3/data/repository/Repository.kt
@@ -0,0 +1,35 @@
+package com.example.composepaging3.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.composepaging3.data.local.UnsplashDatabase
+import com.example.composepaging3.data.model.UnsplashImage
+import com.example.composepaging3.data.paging.UnsplashRemoteMediator
+import com.example.composepaging3.data.remote.UnsplashApi
+import com.example.composepaging3.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class Repository @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val unsplashDatabase: UnsplashDatabase
+) {
+
+    fun getAllImages(): Flow<PagingData<UnsplashImage>> {
+        val pagingSourceFactory = { unsplashDatabase.unsplashImageDao().getAllImages() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = UnsplashRemoteMediator(
+                unsplashApi = unsplashApi,
+                unsplashDatabase = unsplashDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+
+
+}
